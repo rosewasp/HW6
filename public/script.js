7b441fc45bc728b0a2de05c222d0b487d728ab0d
rosewasp <55768481+rosewasp@users.noreply.github.com>
2020-05-29 16:53:56 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,69 +4,71 @@
 
 // connect "Update Workout Log" button to...
 // "/insert" to database
-document.getElementById("submit").addEventListener("click", function(event) {
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("submit").addEventListener("click", function(event) {
   
-  var req = new XMLHttpRequest(); // variable needed to make an Ajax request
-
-  // get all the values from the workout log form
-  var name = document.getElementById("name").value
-  var reps = document.getElementById("reps").value
-  var weight = document.getElementById("weight").value
-  var date = document.getElementById("date").value
+    var req = new XMLHttpRequest(); // variable needed to make an Ajax request
   
-  // Boolean has to be processed differently
-  // for it to take effect in database
-  var trueFalse = document.getElementById("lbs").checked
-  if (trueFalse){
-    var lbs = 1
-  } else {
-    var lbs = 0
-  }
-  
-  // workouts cannot be logged without a name
-  if (name == "") {
-    return;
-  }
-
-  // transform form elements into /insert url
-  var route = "/insert?"
-  var nameUrl = "name"+"="+name+"&"
-  var repsUrl = "reps"+"="+reps+"&"
-  var weightUrl = "weight"+"="+weight+"&"
-  var dateUrl = "date"+"="+date+"&"
-  var lbsUrl = "lbs"+"="+lbs
-  var insertUrl = route + nameUrl + repsUrl + weightUrl + dateUrl + lbsUrl
-
-  // add to database
-  req.open("GET", insertUrl, true);
-
-  req.addEventListener("load", function() {
-    if (req.status >= 200 && req.status < 400) {
-      var response = JSON.parse(req.responseText);
-
-      var req = new XMLHttpRequest();
-      req.open("GET", "/database", true);
-
-      req.addEventListener("load", function(){
-        if (req.status >= 200 && req.status < 400){
-          var response = JSON.parse(req.responseText);
-          var response = JSON.parse(response.results);
-          document.getElementById("results").textContent = response;
-        } else {
-          console.log("Error in network request: " + req.statusText);
-        }
-    });
-    req.send(null);
-    event.preventDefault();
-
+    // get all the values from the workout log form
+    var name = document.getElementById("name").value
+    var reps = document.getElementById("reps").value
+    var weight = document.getElementById("weight").value
+    var date = document.getElementById("date").value
+    
+    // Boolean has to be processed differently
+    // for it to take effect in database
+    var trueFalse = document.getElementById("lbs").checked
+    if (trueFalse){
+      var lbs = 1
     } else {
-      console.log("Error in network request: " + req.statusText);
+      var lbs = 0
     }
+    
+    // workouts cannot be logged without a name
+    if (name == "") {
+      return;
+    }
+  
+    // transform form elements into /insert url
+    var route = "/insert?"
+    var nameUrl = "name"+"="+name+"&"
+    var repsUrl = "reps"+"="+reps+"&"
+    var weightUrl = "weight"+"="+weight+"&"
+    var dateUrl = "date"+"="+date+"&"
+    var lbsUrl = "lbs"+"="+lbs
+    var insertUrl = route + nameUrl + repsUrl + weightUrl + dateUrl + lbsUrl
+  
+    // add to database
+    req.open("GET", insertUrl, true);
+  
+    req.addEventListener("load", function() {
+      if (req.status >= 200 && req.status < 400) {
+        var response = JSON.parse(req.responseText);
+  
+        var req = new XMLHttpRequest();
+        req.open("GET", "/database", true);
+  
+        req.addEventListener("load", function(){
+          if (req.status >= 200 && req.status < 400){
+            var response = JSON.parse(req.responseText);
+            var response = JSON.parse(response.results);
+            document.getElementById("results").textContent = response;
+          } else {
+            console.log("Error in network request: " + req.statusText);
+          }
+      });
+      req.send(null);
+      event.preventDefault();
+  
+      } else {
+        console.log("Error in network request: " + req.statusText);
+      }
+    });
+  
+    req.send(null);
+  
+    event.preventDefault();
   });
-
-  req.send(null);
-
-  event.preventDefault();
 });
 // form a table from each row in database
 // each table row will have edit and delete button
