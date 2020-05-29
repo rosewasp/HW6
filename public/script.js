41f015056e95e2d55bb2109825a76aa41494afb3
rosewasp <55768481+rosewasp@users.noreply.github.com>
2020-05-29 14:39:24 -0700

    multiple

--- public/script.js
+++ public/script.js
@@ -1,13 +1,54 @@
 // most of the code from DOM and Events Assignment...
-// and from XML request lecture
+// and from XML request lecture and Activity.
 
 
 // connect "Update Workout Log" button to...
 // "/insert" to database
+document.getElementById("submit").addEventListener("click", function(event) {
+  
+  var req = new XMLHttpRequest(); // variable needed to make an Ajax request
 
+  // get all the values from the workout log form
+  var name = document.getElementById("name").value
+  var reps = document.getElementById("reps").value
+  var weight = document.getElementById("weight").value
+  var date = document.getElementById("date").value
+  var lbs = document.getElementById("lbs").checked // returns 0 for false and 1 for true
+
+  // workouts cannot be logged without a name
+  if (name == "") {
+    return;
+  }
+
+  // transform form elements into /insert url
+  var route = "/insert?"
+  var nameUrl = "name"+"="+name+"&"
+  var repsUrl = "reps"+"="+reps+"&"
+  var weightUrl = "weight"+"="+weight+"&"
+  var dateUrl = "date"+"="+date+"&"
+  var lbsUrl = "lbs"+"="+lbs
+  var insertUrl = route + nameUrl + repsUrl + weightUrl + dateUrl + lbsUrl
+
+  req.open("GET", insertUrl, true);
+
+  req.addEventListener("load", function() {
+    if (req.status >= 200 && req.status < 400) {
+      var response = JSON.parse(req.responseText);
+      document.getElementById("results").textContent = response;
+    } else {
+      console.log("Error in network request: " + req.statusText);
+    }
+  });
+
+  req.send(null);
+
+  event.preventDefault();
+});
 // form a table from each row in database
 // each table row will have edit and delete button
 
+
+/*
 
 // Create table node
 var fourByFour = document.createElement("table");
@@ -171,3 +212,5 @@ function markYellow() {
 }
 
 document.getElementById("mark").addEventListener("click", markYellow);
+
+*/
